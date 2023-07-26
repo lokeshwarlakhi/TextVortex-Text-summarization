0a7ce1abc3339f78da000cbd106f2fa14af5dc1d
lokeshwarlakhi <lokeshwarlakhi@gmail.com>
2023-07-26 05:48:29 +0530

    data ingestion added

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # TextVortex-Text-summarization
+## work flow
+
+1. update config.yaml file
+2. update params.yaml
+3. update enitity
+4. update configuration manager in src config
+5. update conponents 
+6. update pipeline
+7. update the main.py
+8. update the app.py
---
+++ main.py
@@ -0,0 +1,13 @@
+from TextVortex.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from TextVortex.logging import logger
+
+STAGE_NAME = "data ingestion Stage"
+try:
+    logger.info(f">>>>>>>> stage {STAGE_NAME} started <<<<<<<<")
+    data_ingestion = DataIngestionTrainingPipeline()
+    data_ingestion.main()
+    logger.info(f">>>>>>>>>> stage{STAGE_NAME} completed <<<<<<<\n\n x==============x")
+
+except Exception as e:
+    logger.exception(e)
+    raise e
