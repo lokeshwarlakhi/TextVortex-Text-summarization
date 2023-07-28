d68c23086a81c5091522cac7e411de1de6b44039
lokeshwarlakhi <lokeshwarlakhi@gmail.com>
2023-07-28 18:33:10 +0530

    added data transformation and started building model trainer

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from TextVortex.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from TextVortex.logging import logger
 from TextVortex.pipeline.stage_02_data_validation import DatavalidationtTrainingPipeline
+from TextVortex.pipeline.stage_03_data_transformation import DataTransformationTrainingPipeline
 
 STAGE_NAME = "data ingestion Stage"
 try:
@@ -14,11 +15,22 @@ except Exception as e:
     raise e
 
 
-STAGE_NAME = "data Validation Stage"
+STAGE_NAME = "Data Validation Stage"
 try:
     logger.info(f">>>>>>>> stage {STAGE_NAME} started <<<<<<<<")
     data_validation = DataIngestionTrainingPipeline()
     data_validation.main()
+    logger.info(f">>>>>>>>>> stage{STAGE_NAME} completed <<<<<<<\n\n x==============x")
+
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+STAGE_NAME = "Data Transformation Stage"
+try:
+    logger.info(f">>>>>>>> stage {STAGE_NAME} started <<<<<<<<")
+    data_transformation = DataTransformationTrainingPipeline()
+    data_transformation.main()
     logger.info(f">>>>>>>>>> stage{STAGE_NAME} completed <<<<<<<\n\n x==============x")
 
 except Exception as e:
